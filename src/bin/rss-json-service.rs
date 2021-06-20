087253a9001c7b8a76437df97993a1a88bbe1477
Hannes Hochreiner <hannes@hochreiner.net>
2021-06-20 17:41:20 +0200

    improve error handling

--- src/bin/rss-json-service.rs
+++ src/bin/rss-json-service.rs
@@ -1,12 +1,13 @@
 #[macro_use]
 extern crate rocket;
 extern crate rss_json_service;
-use hyper::{body::Bytes, body::HttpBody as _, Client as HttpClient, StatusCode};
+use hyper::{Client as HttpClient, StatusCode, body::Bytes, body::HttpBody as _, header::ToStrError, http::uri::InvalidUri};
 use hyper_tls::HttpsConnector;
-use rocket::{response::stream::ByteStream, serde::json::Json, State};
+use rocket::{Request, State, http::Status, response::Responder, response::stream::ByteStream, response, serde::json::Json};
 use rss_json_service::repo::{channel::Channel, item::Item, Repo};
 use std::{env, str};
 use uuid::Uuid;
+use log::{error};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -14,27 +15,27 @@ fn index() -> &'static str {
 }
 
 #[get("/channels")]
-async fn channels(repo: &State<Repo>) -> Json<Vec<Channel>> {
-    let channels = repo.get_all_channels().await.unwrap();
+async fn channels(repo: &State<Repo>) -> Result<Json<Vec<Channel>>, CustomError> {
+    let channels = repo.get_all_channels().await?;
 
-    Json(channels)
+    Ok(Json(channels))
 }
 
 #[get("/channels/<channel_id>/items")]
-async fn channel_items(repo: &State<Repo>, channel_id: &str) -> Json<Vec<Item>> {
-    let channel_id = Uuid::parse_str(channel_id).unwrap();
+async fn channel_items(repo: &State<Repo>, channel_id: &str) -> Result<Json<Vec<Item>>, CustomError> {
+    let channel_id = Uuid::parse_str(channel_id)?;
 
-    Json(repo.get_items_by_channel_id(&channel_id).await.unwrap())
+    Ok(Json(repo.get_items_by_channel_id(&channel_id).await?))
 }
 
 #[get("/items/<item_id>/stream")]
-async fn item_stream(repo: &State<Repo>, item_id: &str) -> ByteStream![Bytes] {
-    let item_id = Uuid::parse_str(item_id).unwrap();
-    let item = repo.get_item_by_id(&item_id).await.unwrap();
+async fn item_stream(repo: &State<Repo>, item_id: &str) -> Result<ByteStream![Bytes], CustomError> {
+    let item_id = Uuid::parse_str(item_id)?;
+    let item = repo.get_item_by_id(&item_id).await?;
     let https = HttpsConnector::new();
     let http_client = HttpClient::builder().build::<_, hyper::Body>(https);
     let mut res = http_client
-        .get(item.enclosure_url.parse().unwrap())
+        .get(item.enclosure_url.parse()?)
         .await
         .unwrap();
 
@@ -44,24 +45,24 @@ async fn item_stream(repo: &State<Repo>, item_id: &str) -> ByteStream![Bytes] {
         | StatusCode::TEMPORARY_REDIRECT
         | StatusCode::PERMANENT_REDIRECT => {
             res = http_client
-                .get(res.headers()["location"].to_str().unwrap().parse().unwrap())
-                .await
-                .unwrap();
+                .get(res.headers()["location"].to_str()?.parse()?)
+                .await?;
         }
         StatusCode::OK => {}
         _ => {}
     }
 
-    ByteStream! {
+    Ok(ByteStream! {
         while let Some(next) = res.data().await {
             let chunk = next.unwrap();
             yield chunk;
         }
-    }
+    })
 }
 
 #[launch]
 async fn rocket() -> _ {
+    env_logger::init();
     let repo = Repo::new(&*format!(
         "postgresql://{}:{}@localhost:5432/rss_json",
         env::var("RSS_JSON_USER").unwrap(),
@@ -73,4 +74,46 @@ async fn rocket() -> _ {
     rocket::build()
         .manage(repo)
         .mount("/", routes![index, channels, channel_items, item_stream])
+}
+
+struct CustomError {
+    msg: String,
+}
+
+impl<'r, 'o: 'r> Responder<'r, 'o> for CustomError
+{
+    fn respond_to(self, _: &'r Request<'_>) -> response::Result<'o> {
+        error!("{}", self.msg);
+        Err(Status::InternalServerError)
+    }
+}
+
+impl std::convert::From<anyhow::Error> for CustomError {
+    fn from(e: anyhow::Error) -> Self {
+        CustomError { msg: format!("{}", e) }
+    }
+}
+
+impl std::convert::From<uuid::Error> for CustomError {
+    fn from(e: uuid::Error) -> Self {
+        CustomError { msg: format!("{}", e) }
+    }
+}
+
+impl std::convert::From<ToStrError> for CustomError {
+    fn from(e: ToStrError) -> Self {
+        CustomError { msg: format!("{}", e) }
+    }
+}
+
+impl std::convert::From<InvalidUri> for CustomError {
+    fn from(e: InvalidUri) -> Self {
+        CustomError { msg: format!("{}", e) }
+    }
+}
+
+impl std::convert::From<hyper::Error> for CustomError {
+    fn from(e: hyper::Error) -> Self {
+        CustomError { msg: format!("{}", e) }
+    }
 }
